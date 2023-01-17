59e58e6f7a5c76cc0937b943dea119ac12a124df
Mohamed-Khaterr <mohamed.15751@gmail.com>
2023-01-17 03:03:28 +0200

    initial commit

---
+++ swift-login-system/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  swift-login-system
+//
+//  Created by Khater on 1/17/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+}
---
+++ swift-login-system/RegisterViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RegisterViewController.swift
+//  swift-login-system
+//
+//  Created by Khater on 1/17/23.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
