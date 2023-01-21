78720b093746bdd42d1e16fb7672f2badfb1da16
Mohamed-Khaterr <mohamed.15751@gmail.com>
2023-01-22 01:36:45 +0200

    refactor LoginVC

--- swift-login-system/Controller/LoginController.swift
+++ swift-login-system/Controller/LoginController.swift
@@ -12,7 +12,7 @@ import GoogleSignIn
 class LoginController: UIViewController {
     
     // MARK: - UI Components
-    private let mainView = LoginView()
+    private lazy var mainView = LoginView(delegate: self, facebookDeleage: self, facebookPermissions: ["email", "public_profile"])
 
     
     // MARK: - LifeCycle
@@ -25,133 +25,126 @@ class LoginController: UIViewController {
         super.viewDidLoad()
         self.view.backgroundColor = .systemBackground
         self.navigationItem.title = "Sign In"
-        
-//        facebookSignInButton.permissions = ["email", "public_profile"]
-//        facebookSignInButton.delegate = self
-//
-//        forgotPasswordButton.addTarget(self, action: #selector(forgotPasswordButtonPressed), for: .touchUpInside)
-//        signInButton.addTarget(self, action: #selector(signInButtonPressed), for: .touchUpInside)
-//        facebookSignInButton.addTarget(self, action: #selector(facebookButtonPressed), for: .touchUpInside)
-//        googleSignInButton.addTarget(self, action: #selector(googleButtonPressed), for: .touchUpInside)
-//        createAccountButton.addTarget(self, action: #selector(createAccountButtonPressed), for: .touchUpInside)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.isHidden = true
+    }
+}
+
+
+// MARK: - LoginView Delegate
+extension LoginController: LoginViewDelegate {
+    func forgotPasswordButtonPressed() {
+        navigationController?.pushViewController(ForgotPasswordViewController(), animated: true)
     }
     
+    func signInButtonPressed() {
+        let email = mainView.email
+        let password = mainView.password
+        
+        guard !email.isEmpty, !password.isEmpty else {
+            AlertManager.show(to: self, withTitle: "Sign In", andMessage: "Please fill all fields,\nand Try Again.")
+            return
+        }
+        
+        // Email Check
+        if !Validator.isValidEmail(for: email.lowercased()){
+            AlertManager.show(to: self, withTitle: "Invalid Email", andMessage: "Email is not valid,\nTry Again.")
+            return
+        }
+        
+        mainView.enabaleViews(false)
+        
+        let user = LoginUserRequest(email: email.lowercased(),
+                                    password: password)
+        
+        AuthService.shared.singIn(withUser: user) { [weak self] error in
+            guard let self = self else { return }
+            self.mainView.enabaleViews(true)
+            if let error = error {
+                AlertManager.show(to: self, withTitle: "Sign In", andMessage: error.localizedDescription, returnKey: "Dismiss")
+                return
+            }
+            
+            if let sceneDelegate = self.view.window?.windowScene?.delegate as? SceneDelegate {
+                sceneDelegate.goToRootVC()
+            }
+        }
+    }
     
-    // MARK: - Selectors
-//    @objc private func forgotPasswordButtonPressed(){
-//        navigationController?.pushViewController(ForgotPasswordViewController(), animated: true)
-//    }
+    func facebookButtonPressed() {
+        mainView.enabaleViews(false)
+    }
     
-//    @objc private func signInButtonPressed(){
-//        guard let email = emailTextField.text, !email.isEmpty,
-//              let password = passwordTextField.text, !password.isEmpty
-//        else {
-//            AlertManager.show(to: self, withTitle: "Sign In", andMessage: "Please fill all fields,\nand Try Again.")
-//            return
-//        }
-//
-//        // Email Check
-//        if !Validator.isValidEmail(for: email.lowercased()){
-//            AlertManager.show(to: self, withTitle: "Invalid Email", andMessage: "Email is not valid,\nTry Again.")
-//            return
-//        }
-//
-//        enabaleViews(false)
-//
-//        let user = LoginUserRequest(email: email.lowercased(),
-//                                    password: password)
-//
-//        AuthService.shared.singIn(withUser: user) { [weak self] error in
-//            guard let self = self else { return }
-//            self.enabaleViews(true)
-//            if let error = error {
-//                AlertManager.show(to: self, withTitle: "Sign In", andMessage: error.localizedDescription, returnKey: "Dismiss")
-//                return
-//            }
-//
-//            if let sceneDelegate = self.view.window?.windowScene?.delegate as? SceneDelegate {
-//                sceneDelegate.goToRootVC()
-//            }
-//        }
-//    }
+    func googleButtonPressed() {
+        mainView.enabaleViews(false)
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { [weak self] signInResult, error in
+            guard let self = self else { return }
+            self.mainView.enabaleViews(true)
+            
+            if let error = error {
+                AlertManager.show(to: self, withTitle: "Google Sign In", andMessage: error.localizedDescription, returnKey: "Dismiss")
+                return
+            }
+            
+            guard
+                let user = signInResult?.user,
+                let idToken = user.idToken
+            else { return }
+            
+            let accessToken = user.accessToken
+            
+            AuthService.shared.signIn(withProvidere: .google(idToken: idToken.tokenString, accessToken: accessToken.tokenString)) { signInSuccess, error in
+                if let error = error {
+                    AlertManager.show(to: self, withTitle: "Google Sign In", andMessage: error.localizedDescription, returnKey: "Dismiss")
+                    return
+                }
+                
+                if signInSuccess {
+                    if let sceneDelegate = self.view.window?.windowScene?.delegate as? SceneDelegate {
+                        sceneDelegate.goToRootVC()
+                    }
+                }
+            }
+        }
+    }
     
-//    @objc private func facebookButtonPressed(){
-//        enabaleViews(false)
-//    }
-    
-    
-//    @objc private func googleButtonPressed(){
-//        enabaleViews(false)
-//        GIDSignIn.sharedInstance.signIn(withPresenting: self) { [weak self] signInResult, error in
-//            guard let self = self else { return }
-//            self.enabaleViews(true)
-//
-//            if let error = error {
-//                AlertManager.show(to: self, withTitle: "Google Sign In", andMessage: error.localizedDescription, returnKey: "Dismiss")
-//                return
-//            }
-//
-//            guard
-//                let user = signInResult?.user,
-//                let idToken = user.idToken
-//            else { return }
-//
-//            let accessToken = user.accessToken
-//
-//            AuthService.shared.signIn(withProvidere: .google(idToken: idToken.tokenString, accessToken: accessToken.tokenString)) { signInSuccess, error in
-//                if let error = error {
-//                    AlertManager.show(to: self, withTitle: "Google Sign In", andMessage: error.localizedDescription, returnKey: "Dismiss")
-//                    return
-//                }
-//
-//                if signInSuccess {
-//                    if let sceneDelegate = self.view.window?.windowScene?.delegate as? SceneDelegate {
-//                        sceneDelegate.goToRootVC()
-//                    }
-//                }
-//            }
-//        }
-//    }
-    
-//    @objc private func createAccountButtonPressed(){
-//        self.navigationController?.pushViewController(RegisterViewController(), animated: true)
-//    }
+    func createAccountButtonPresssed() {
+        navigationController?.pushViewController(RegisterViewController(), animated: true)
+    }
 }
 
 
 // MARK: - FacebookSignInButton Delegate
-//extension LoginViewController: LoginButtonDelegate{
-//    func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
-//        if let error = error {
-//            AlertManager.show(to: self, withTitle: "Facebook Login", andMessage: error.localizedDescription, returnKey: "Dismiss")
-//            return
-//        }
-//
-//        guard let accessToken = result?.token else { return }
-//
-//        AuthService.shared.signIn(withProvidere: .facebook(accessToken: accessToken.tokenString)) { [weak self] signInSuccess, error in
-//            guard let self = self else { return }
-//            self.enabaleViews(true)
-//
-//            if let error = error {
-//                AlertManager.show(to: self, withTitle: "Facebook Login", andMessage: error.localizedDescription, returnKey: "Dismiss")
-//                return
-//            }
-//
-//            if signInSuccess {
-//                if let sceneDelegate = self.view.window?.windowScene?.delegate as? SceneDelegate {
-//                    sceneDelegate.goToRootVC()
-//                }
-//            }
-//        }
-//    }
-//
-//    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
-//        print("Logout")
-//    }
-//}
+extension LoginController: LoginButtonDelegate{
+    func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
+        if let error = error {
+            AlertManager.show(to: self, withTitle: "Facebook Login", andMessage: error.localizedDescription, returnKey: "Dismiss")
+            return
+        }
+
+        guard let accessToken = result?.token else { return }
+
+        AuthService.shared.signIn(withProvidere: .facebook(accessToken: accessToken.tokenString)) { [weak self] signInSuccess, error in
+            guard let self = self else { return }
+            self.mainView.enabaleViews(true)
+
+            if let error = error {
+                AlertManager.show(to: self, withTitle: "Facebook Login", andMessage: error.localizedDescription, returnKey: "Dismiss")
+                return
+            }
+
+            if signInSuccess {
+                if let sceneDelegate = self.view.window?.windowScene?.delegate as? SceneDelegate {
+                    sceneDelegate.goToRootVC()
+                }
+            }
+        }
+    }
+
+    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
+        print("Logout")
+    }
+}
--- swift-login-system/View/LoginView.swift
+++ swift-login-system/View/LoginView.swift
@@ -6,15 +6,267 @@
 //
 
 import UIKit
+import FacebookLogin
+import GoogleSignIn
+
+@objc protocol LoginViewDelegate: AnyObject {
+    @objc func forgotPasswordButtonPressed()
+    @objc func signInButtonPressed()
+    @objc func facebookButtonPressed()
+    @objc func googleButtonPressed()
+    @objc func createAccountButtonPresssed()
+}
 
 class LoginView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    // MARK: - Variables
+    private weak var delegate: LoginViewDelegate!
+    
+    public var email: String {
+        return emailTextField.text ?? ""
     }
-    */
-
+    
+    public var password: String {
+        return passwordTextField.text ?? ""
+    }
+    
+    // MARK: - UI Components
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.showsVerticalScrollIndicator = false
+        return scrollView
+    }()
+    
+    private let containerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let headerView = CustomHeaderView(title: "Sign In", subTitle: "Sign in to your account")
+    private let emailTextField = CustomTextField(textFieldType: .email)
+    private let passwordTextField = CustomTextField(textFieldType: .password)
+    
+    private lazy var forgotPasswordButton: CustomButton = {
+        let button = CustomButton(title: "Forgot Password?", fontSize: .small)
+        button.addTarget(delegate, action: #selector(delegate.forgotPasswordButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private let loadingIndicator: UIActivityIndicatorView = {
+        let activityIndicator = UIActivityIndicatorView()
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        activityIndicator.color = .label
+        activityIndicator.style = .medium
+        activityIndicator.hidesWhenStopped = true
+        return activityIndicator
+    }()
+    
+    private lazy var signInButton: CustomButton = {
+        let button = CustomButton(title: "Sign In", hasBackground: true, fontSize: .medium)
+        button.addTarget(delegate, action: #selector(delegate.signInButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var facebookSignInButton: FBLoginButton = {
+        let button = FBLoginButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(delegate, action: #selector(delegate.facebookButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var googleSignInButton: GIDSignInButton = {
+        let button = GIDSignInButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(delegate, action: #selector(delegate.googleButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var createAccountButton: CustomButton = {
+        let button = CustomButton(title: "Don't have account? Create Account", fontSize: .small)
+        button.addTarget(delegate, action: #selector(delegate.createAccountButtonPresssed), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - LifeCycle
+    init(delegate: LoginViewDelegate, facebookDeleage: LoginButtonDelegate, facebookPermissions: [String]){
+        super.init(frame: .zero)
+        self.backgroundColor = .systemBackground
+        
+        self.delegate = delegate
+        self.facebookSignInButton.delegate = facebookDeleage
+        self.facebookSignInButton.permissions = facebookPermissions
+        
+        addSubviews()
+        layoutUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        // This will make fatal error if we try to use This View in storyboard
+        fatalError("init(coder:) has not been implemented")
+        
+        // If we want to use This View in stroyboard (This View Support everything)
+        //super.init(coder: coder)
+    }
+    
+    
+    
+    // MARK: - AddSubviews
+    private func addSubviews(){
+        self.addSubview(scrollView)
+        scrollView.addSubview(containerView)
+        containerView.addSubview(headerView)
+        containerView.addSubview(emailTextField)
+        containerView.addSubview(passwordTextField)
+        containerView.addSubview(forgotPasswordButton)
+        containerView.addSubview(loadingIndicator)
+        containerView.addSubview(signInButton)
+        containerView.addSubview(facebookSignInButton)
+        containerView.addSubview(googleSignInButton)
+        containerView.addSubview(createAccountButton)
+    }
+    
+    
+    
+    // MARK: - LayoutUI
+    private func layoutUI() {
+        setupScrollViewConstraints()
+        setupContainerViewConstraints()
+        setupHeaderViewConstraints()
+        setupEmailTextFieldConstraints()
+        setupPasswordTextFieldConstraints()
+        setupForgotPasswordButtonConstraints()
+        setupLoadingIndicatorConstraints()
+        setupSignInButtonConstraints()
+        setupFacebookButtonConstraints()
+        setupGoogleButtonConstraints()
+        setupCreateAccountButtonConstraints()
+    }
+    
+    private func setupScrollViewConstraints(){
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
+    
+    private func setupContainerViewConstraints(){
+        NSLayoutConstraint.activate([
+            containerView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            containerView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            containerView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            containerView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+        ])
+    }
+    
+    private func setupHeaderViewConstraints(){
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 30),
+            headerView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 180),
+        ])
+    }
+    
+    private func setupEmailTextFieldConstraints(){
+        NSLayoutConstraint.activate([
+            emailTextField.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: 16),
+            emailTextField.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            emailTextField.widthAnchor.constraint(equalTo: containerView.widthAnchor, multiplier: 0.8),
+            emailTextField.heightAnchor.constraint(equalToConstant: 55),
+        ])
+    }
+    
+    private func setupPasswordTextFieldConstraints(){
+        NSLayoutConstraint.activate([
+            passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 16),
+            passwordTextField.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            passwordTextField.widthAnchor.constraint(equalTo: containerView.widthAnchor, multiplier: 0.8),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 55),
+        ])
+    }
+    
+    private func setupForgotPasswordButtonConstraints(){
+        NSLayoutConstraint.activate([
+            forgotPasswordButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor),
+            forgotPasswordButton.trailingAnchor.constraint(equalTo: passwordTextField.trailingAnchor),
+        ])
+    }
+    
+    private func setupLoadingIndicatorConstraints(){
+        NSLayoutConstraint.activate([
+            loadingIndicator.topAnchor.constraint(equalTo: forgotPasswordButton.bottomAnchor, constant: 26),
+            loadingIndicator.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+        ])
+    }
+    
+    private func setupSignInButtonConstraints(){
+        NSLayoutConstraint.activate([
+            signInButton.topAnchor.constraint(equalTo: forgotPasswordButton.bottomAnchor, constant: 24),
+            signInButton.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            signInButton.widthAnchor.constraint(equalTo: containerView.widthAnchor, multiplier: 0.8),
+            signInButton.heightAnchor.constraint(equalToConstant: 55),
+        ])
+    }
+    
+    private func setupFacebookButtonConstraints(){
+        NSLayoutConstraint.activate([
+            facebookSignInButton.topAnchor.constraint(equalTo: signInButton.bottomAnchor, constant: 24),
+            facebookSignInButton.widthAnchor.constraint(equalTo: containerView.widthAnchor, multiplier: 0.8),
+            facebookSignInButton.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+        ])
+    }
+    
+    private func setupGoogleButtonConstraints(){
+        NSLayoutConstraint.activate([
+            googleSignInButton.topAnchor.constraint(equalTo: facebookSignInButton.bottomAnchor, constant: 18),
+            googleSignInButton.widthAnchor.constraint(equalTo: containerView.widthAnchor, multiplier: 0.8),
+            googleSignInButton.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+        ])
+    }
+    
+    private func setupCreateAccountButtonConstraints(){
+        NSLayoutConstraint.activate([
+            createAccountButton.topAnchor.constraint(greaterThanOrEqualTo: googleSignInButton.bottomAnchor, constant: 40),
+            createAccountButton.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            createAccountButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -12),
+        ])
+    }
+    
+    
+    
+    
+    // MARK: - UpadteUI Functions
+    public func enabaleViews(_ enable: Bool){
+        DispatchQueue.main.async {
+            self.emailTextField.isEnabled = enable
+            self.passwordTextField.isEnabled = enable
+            self.forgotPasswordButton.isEnabled = enable
+            self.signInButton.isEnabled = enable
+            self.facebookSignInButton.isEnabled = enable
+            self.googleSignInButton.isEnabled = enable
+            self.createAccountButton.isEnabled = enable
+            
+            
+            if !enable {
+                self.loadingIndicator.startAnimating()
+                UIView.animate(withDuration: 0.5) {
+                    self.signInButton.transform.ty = 34
+                    self.facebookSignInButton.transform.ty = 28
+                    self.googleSignInButton.transform.ty = 28
+                }
+            }else{
+                self.loadingIndicator.stopAnimating()
+                UIView.animate(withDuration: 0.3) {
+                    self.signInButton.transform = .identity
+                    self.facebookSignInButton.transform = .identity
+                    self.googleSignInButton.transform = .identity
+                }
+            }
+        }
+    }
 }
